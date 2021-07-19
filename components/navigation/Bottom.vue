7017a0a09ea8131d12ead9059ffbc21ab9558071
devdef20@gmail.com <devdef20@gmail.com>
2021-07-19 13:42:01 +0700

    Updates

--- components/navigation/Bottom.vue
+++ components/navigation/Bottom.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="navigation-bottom inline mx-auto text-center">
     <div class="flex justify-center py-3">
-      <p>
-        Copyrights Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Debitis ipsa fugit quod velit!.
-      </p>
+      <p>All right reserved &copy; 2021.</p>
     </div>
     <div class="block container mx-auto">
       <ColorMode />
